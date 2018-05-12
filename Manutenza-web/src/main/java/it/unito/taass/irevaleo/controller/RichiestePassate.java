62049332942a97d4c9f91445a3d8212e4ea47880
leonardo <leonardo@mbp-di-leonardo.lan>
2018-05-12 09:39:57 +0200

    aggiunta servlet RichiestePassate

---
+++ Manutenza-web/src/main/java/it/unito/taass/irevaleo/controller/RichiestePassate.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unito.taass.irevaleo.controller;
+
+import it.unito.taass.irevaleo.Utilita;
+import it.unito.taass.manutenza.ejb.GestoreRichiesteLocal;
+import it.unito.taass.manutenza.entities.Richiesta;
+import it.unito.taass.manutenza.entities.Utente;
+import java.io.IOException;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author leonardo
+ */
+public class RichiestePassate extends HttpServlet {
+    
+    @EJB(beanName = "GestoreRichieste")
+    private GestoreRichiesteLocal gestoreRichieste;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession s = request.getSession();
+        ServletContext ctx = getServletContext();
+        
+        Utente utente = (Utente)s.getAttribute("utente");
+        
+        /* ELENCO DELLE RICHIESTE COMPLETATE*/
+        List<Richiesta> listaRichiesteCompletate = this.gestoreRichieste.cercaRichieste(utente, Utilita.COMPLETATA);
+        List<Richiesta> listaRichiesteValutate = this.gestoreRichieste.cercaRichieste(utente, Utilita.VALUTATA);
+        
+        request.setAttribute("richiesteCompletate", listaRichiesteCompletate);
+        request.setAttribute("richiesteValutate", listaRichiesteValutate);
+        
+        ctx.getRequestDispatcher("/jsp/richiesteAccettate.jsp").forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
